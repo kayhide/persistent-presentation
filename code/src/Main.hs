ba2d74a1d0ce5c4e54e69911ab19bf91694b58b4
kayhide <kayhide@gmail.com>
2016-08-22 23:55:05 +0900

    update to read Chinook.

--- code/src/Main.hs
+++ code/src/Main.hs
@@ -15,31 +15,24 @@ import           Database.Persist.Sqlite
 import           Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Person
+Artist
+    Id sql=ArtistId
     name String
-    age Int Maybe
     deriving Show
-BlogPost
+Album
+    Id sql=AlbumId
     title String
-    authorId PersonId
+    artistId ArtistId sql=ArtistId
     deriving Show
 |]
 
+
+printAlbum artist = do
+  albums <- selectList [AlbumArtistId ==. artist] [LimitTo 10]
+  liftIO $ print albums
+
 main :: IO ()
-main = runSqlite ":memory:" $ do
-    runMigration migrateAll
-
-    johnId <- insert $ Person "John Doe" $ Just 35
-    janeId <- insert $ Person "Jane Doe" Nothing
-
-    insert $ BlogPost "My fr1st p0st" johnId
-    insert $ BlogPost "One more for good measure" johnId
-
-    oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
-    liftIO $ print (oneJohnPost :: [Entity BlogPost])
-
-    john <- get johnId
-    liftIO $ print (john :: Maybe Person)
-
-    delete janeId
-    deleteWhere [BlogPostAuthorId ==. johnId]
+main = runSqlite "Chinook_Sqlite.sqlite" $ do
+  artists <- selectList ([] :: [Filter Artist]) [LimitTo 10]
+  liftIO $ print artists
+  mapM_ (printAlbum . entityKey) $ artists
